cf445d26ae8ae7910224252f0854391d72a482b4
Sam Podlogar <sam@podlogar.com>
2013-10-29 13:16:29 -0500

    working on gravtar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -33,11 +33,11 @@ module ApplicationHelper
   def get_avtar
     if current_user
       if current_user.avatar?
-        retern image_tag(current_user.avatar.tiny.url)
+        image_tag(current_user.avatar.tiny.url)
       else
         id = Digest::MD5::hexdigest(current_user.email.downcase)
         url = "https://secure.gravatar.com/avatar/#{id}.png"
-        return image_tag(url, width: "20px", height: "20px")
+        image_tag(url, width: "20px", height: "20px")
       end
     end
   end
