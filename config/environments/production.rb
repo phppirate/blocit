13ff16ecd97fd32f11a2d5f1983c47f2fb5e3d30
Sam Podlogar <sam@podlogar.com>
2013-10-29 13:52:44 -0500

    Fixed sendgrod problem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Bloccit::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   #config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.default_url_options = { :host => 'sam-podlogar.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'blocit.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 end
